c2d8516e3d7e137c65f3e98aeaf243956f93cb0f
Soumyanjalee Mahapatra <soumyanjalee.mahapatra@qunomedical.com>
2018-07-10 13:03:23 +0200

    changed names

--- angular/my-app/src/app/app.component.ts
+++ angular/my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'I think its an Angular App';
+  title = 'I think I am an Angular App';
 }
--- angular/my-app/src/app/mock-heroes.ts
+++ angular/my-app/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero'
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Captain Jack Sparrow' },
+  { id: 12, name: 'Frank Tupelo' },
+  { id: 13, name: 'Alexander Pearce' },
+  { id: 14, name: 'John Dillinger' },
+  { id: 15, name: 'Mad Hatter' },
+  { id: 16, name: 'Roux' },
+  { id: 17, name: 'Tonto' },
+  { id: 18, name: 'Willy Wonka' },
+  { id: 19, name: 'Yogi Victor' },
+  { id: 20, name: 'Cesar' }
 ]
